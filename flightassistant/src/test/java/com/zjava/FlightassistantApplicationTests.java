5ac7864f06c2e0da35ce0731007db973cf68e9ad
Adrian Pusty <apusty@it.p.lodz.pl>
2017-06-02 11:58:23 +0200

    initial commit

---
+++ flightassistant/src/test/java/com/zjava/FlightassistantApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightassistantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
