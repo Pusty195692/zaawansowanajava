00f3701bf2cc20c7839ff5d04cceba488f5383d2
Adrian Pusty <apusty@it.p.lodz.pl>
2017-06-16 16:05:46 +0200

    testy

--- flightassistant/src/test/java/com/zjava/api/ApiTests.java
+++ flightassistant/src/test/java/com/zjava/api/ApiTests.java
@@ -1,6 +1,9 @@
 package com.zjava.api;
 
 import com.zjava.model.AircraftType;
+import com.zjava.model.Airline;
+import com.zjava.model.Destination;
+import com.zjava.model.Flight;
 import org.json.simple.parser.ParseException;
 import org.junit.Test;
 
@@ -24,22 +27,22 @@ public class ApiTests {
     @Test
     public void getAirlines() throws Exception {
         GetData getData = new GetData();
-        List<AircraftType> aircraftTypes = getData.getObjectsList(new AircraftType(), AircraftType.class,  "v1", "aircraftTypes");
-        assertEquals(aircraftTypes.get(0).getClass(), AircraftType.class);
+        List<Airline> aircraftTypes = getData.getObjectsList(new Airline(), Airline.class,  "v1", "airlines");
+        assertEquals(aircraftTypes.get(0).getClass(), Airline.class);
     }
 
     @Test
     public void getDestinations() throws Exception {
         GetData getData = new GetData();
-        List<AircraftType> aircraftTypes = getData.getObjectsList(new AircraftType(), AircraftType.class,  "v1", "aircraftTypes");
-        assertEquals(aircraftTypes.get(0).getClass(), AircraftType.class);
+        List<Destination> aircraftTypes = getData.getObjectsList(new Destination(), Destination.class,  "v1", "destinations");
+        assertEquals(aircraftTypes.get(0).getClass(), Destination.class);
     }
 
     @Test
     public void getFlights() throws Exception {
         GetData getData = new GetData();
-        List<AircraftType> aircraftTypes = getData.getObjectsList(new AircraftType(), AircraftType.class,  "v1", "aircraftTypes");
-        assertEquals(aircraftTypes.get(0).getClass(), AircraftType.class);
+        List<Flight> aircraftTypes = getData.getObjectsList(new Flight(), Flight.class,  "v1", "flights");
+        assertEquals(aircraftTypes.get(0).getClass(), Flight.class);
     }
 
 
