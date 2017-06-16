10a1253440afe354cfbd53c21dcbc17d94076fcb
Piotr Kaźmierczak <195611@edu.p.lodz.pl>
2017-06-16 11:50:38 +0200

    Added ResetToken model & methods for password reseting

--- flightassistant/src/main/java/com/zjava/service/UserService.java
+++ flightassistant/src/main/java/com/zjava/service/UserService.java
@@ -1,22 +1,31 @@
 package com.zjava.service;
 
 import com.zjava.exception.EmailNotUniqueException;
+import com.zjava.model.PasswordResetToken;
 import com.zjava.model.Role;
 import com.zjava.model.User;
+import com.zjava.repository.PasswordResetTokenRepository;
 import com.zjava.repository.RoleRepository;
 import com.zjava.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import javax.inject.Inject;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
-
+import com.zjava.service.EmailService.Message;
 
 /**
  * Created by Rafal Lebioda on 13.06.2017.
@@ -28,6 +37,19 @@ public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordResetTokenRepository tokenRepository;
+
+    @Value("${port:8080}")
+    private String port;
+
+    @Value("${host:localhost}")
+    private String host;
+
+    @Value("${protocol:http}")
+    private String protocol;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public List<User> findAllUsers() {
         log.info("Searching all users");
@@ -83,6 +105,62 @@ public class UserService implements UserDetailsService {
         }
     }
 
+    ////////methods fo password reset
+
+    public String generateToken() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public void createPasswordResetTokenForUser(User user, String token) {
+        PasswordResetToken resetToken = new PasswordResetToken();
+        resetToken.setUser(user);
+        resetToken.setToken(token);
+        resetToken.setExpiryDate(LocalDateTime.now().plusMinutes(30));
+        tokenRepository.save(resetToken);
+    }
+
+    public boolean validatePasswordResetToken(String token, String email) {
+        log.info("validatePasswordResetToken method invoke");
+        PasswordResetToken resetToken = tokenRepository.findByToken(token);
+        if (resetToken == null) {
+            log.info("Token is invalid");
+            return false;
+        }
+
+        User user = resetToken.getUser();
+        if (!user.getEmail().equals(email)) {
+            log.info("Token is invalid");
+            return false;
+        }
+
+        if (resetToken.getExpiryDate().isBefore(LocalDateTime.now())) {
+            log.warn("Token[" + resetToken + "] expired - removing");
+            tokenRepository.delete(resetToken);
+            return false;
+        }
+
+        tokenRepository.delete(resetToken);
+
+        log.info("Token " + token + " is valid");
+        log.info("Token " + token + " removed from database");
+
+        return true;
+    }
+
+    public void changePassword(User user, String password) {
+        User userToUpdate = userRepository.findOne(user.getId());
+        userToUpdate.setPassword(passwordEncoder.encode((password)));
+        userRepository.save(userToUpdate);
+        log.info("User " + user.getUsername() + " changed password for " + password);
+    }
+
+    public SimpleMailMessage constructResetTokenEmail(User user, String token) {
+        List<String> to = new ArrayList<>();
+        to.add(user.getEmail());
+        String url = protocol + "://" + host + ":" + port + "/account/password/change/reset/token/" + token;
+        Message message = new Message("flightassistant@gmail.com", to, "Reset password", url);
+        return message.constructEmail();
+    }
 
 
 
